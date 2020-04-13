9c40aa204c268d94224296a879fc6ce24666c3c7
LinZikai1996 <58392181+LinZikai1996@users.noreply.github.com>
2020-04-14 05:33:13 +0800

    add share shell

---
+++ shell/share.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# 1 get parameter
+parameter_number=$#
+
+if [[ $parameter_number==0 ]]; then
+    echo no parameter
+    exit
+fi
+
+# 2 get file name
+parameter_1=$1
+file_name=`basename $parameter_1`
+echo file_name=$file_name
+
+# 3 get path
+path=`cd -P $(dirname "$parameter_1"); pwd `
+echo path=$path
+
+# 4 user name
+user=`whoami`
+
+# 5 cycle
+
+for (( host = 100; host < 104; host++ )); do
+    echo ------------------------ hadoop$host ------------------------------
+    rsync -av $path/$file_name $user@hadoop$host:$path
+done
